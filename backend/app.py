27385d980e37c02ad4e16938ddaf103e98ca4f95
digit987 <15653107+digit987@users.noreply.github.com>
2024-06-16 20:28:15 +0530

    Set renamed frontend url in app.py

--- backend/app.py
+++ backend/app.py
@@ -3,7 +3,7 @@ from flask_cors import CORS
 from rag_client import invoke_rag_chain
 
 app = Flask(__name__)
-CORS(app, resources={r"/api/*": {"origins": "https://vue-flask-chatbot.netlify.app"}})
+CORS(app, resources={r"/api/*": {"origins": "https://smile2steps-chatbot.netlify.app"}})
 
 @app.route('/api/data', methods=['POST'])
 def get_data():
